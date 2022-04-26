32f2e2205f779e544e9a95a532e02b4e3be9c78d
Mousumi <dutta.mousumi136@gmail.com>
2022-04-25 21:14:43 -0400

    Answer for EventHandler program in Final Test

---
+++ EventTest/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace EvenTtest
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Press T to click");
+            var key = Console.ReadLine();
+            if (key == "t")
+            {
+                Pressed();
+            }
+
+            static void Pressed()
+            {
+                Button button = new Button();
+                button.Click += (s, e) =>
+                {
+                    Console.WriteLine("You clicked a button!");
+                };
+                button.OnClick();
+            }
+
+        }
+    }
+
+   
+ //code for the eventhandler
+    public class Button
+    {
+        public EventHandler Click;
+        public void OnClick()
+        {
+            Click.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+   
+}
